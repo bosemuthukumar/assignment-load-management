e8eff3a3fed265b3a177712755d175d1cf5a8299
muthukumar2022 <kumarmuthu.tech@gmail.com>
2026-02-26 01:42:46 +0530

    deploy vercel commit four

--- backend/src/server.js
+++ backend/src/server.js
@@ -37,7 +37,7 @@ app.use((req, res) => {
 });
 
 // Database connection and server start
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 sequelize
   .authenticate()
